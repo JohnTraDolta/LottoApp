bfea0345488f1391805aebd92c5da10c34c0ada6
Marius <mariusslmoeller@gmail.com>
2020-01-11 22:31:23 +0100

    Add project files.

---
+++ LottoApp/LottoApp.Android/NumberButtonRender.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Reflection;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Graphics.Drawables.Shapes;
+using Android.Text;
+using Android.Views;
+using Java.Nio.Channels;
+using LottoApp;
+using LottoApp.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Platform.Android.FastRenderers;
+using Color = Android.Graphics.Color;
+using FrameRenderer = Xamarin.Forms.Platform.Android.FrameRenderer;
+using View = Android.Views.View;
+
+[assembly: ExportRendererAttribute(typeof(NumberButton), typeof(NumberButtonRender))]
+namespace LottoApp.Droid
+{
+    public sealed class NumberButtonRender : ViewRenderer<NumberButton, View>
+    {
+        
+        public NumberButtonRender(Context context) : base(context)
+        {
+            SetWillNotDraw(false);
+        }
+
+        public override void Draw(Canvas canvas)
+        {
+            NumberButton button = (NumberButton) this.Element;
+
+            Paint backgroundPaint = new Paint()
+            {
+                Color = button.BackgroundColor.ToAndroid(),
+                AntiAlias = true
+            };
+            TextPaint textPaint = new TextPaint()
+            {
+                Color = button.TextColor.ToAndroid(),
+                AntiAlias = true,
+                TextSize = (float)button.FontSize,
+                TextAlign = Paint.Align.Center
+            };
+
+            
+            Rect bounds = new Rect();
+            textPaint.GetTextBounds(button.Text, 0, button.Text.Length, bounds);
+
+            canvas.DrawCircle((float)canvas.Width/2, (float)canvas.Height/2, (float)button.WidthRequest, backgroundPaint);
+            canvas.DrawText(button.Text,
+                (float)canvas.Width/2, 
+                ((float)canvas.Height + bounds.Height())/2 , textPaint);
+
+        }
+
+        public override bool OnTouchEvent(MotionEvent e)
+        {
+            var motionEventActions = e.Action;
+            NumberButton button = (NumberButton)this.Element;
+            switch (motionEventActions)
+            {
+                case MotionEventActions.Down:
+                    var oldBgColor = button.BackgroundColor;
+                    var oldTxtColor = button.TextColor;
+
+                    button.BackgroundColor = button.AlternateBackgroundColor;
+                    button.AlternateBackgroundColor = oldBgColor;
+
+                    button.TextColor = button.AlternateTextColor;
+                    button.AlternateTextColor = oldTxtColor;
+
+                    return false;
+                default:
+                    return base.OnTouchEvent(e);
+            }
+            
+        }
+    }
+
+}
---
+++ LottoApp/LottoApp/NumberButton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace LottoApp
+{
+    public class NumberButton : View
+    {
+        public static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create(nameof(CornerRadiusProperty),typeof(double),typeof(NumberButton), (double)0, BindingMode.Default);
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(TextProperty), typeof(String), typeof(NumberButton), "", BindingMode.Default);
+        public static readonly BindableProperty TextColorProperty = BindableProperty.Create(nameof(TextColorProperty), typeof(Color), typeof(NumberButton), Color.Black, BindingMode.Default);
+        public static readonly BindableProperty FontSizeProperty = BindableProperty.Create(nameof(FontSizeProperty),typeof(double),typeof(NumberButton), (double)0, BindingMode.Default);
+        public static readonly BindableProperty AlternateBackgroundColorProperty = BindableProperty.Create(nameof(AlternateBackgroundColorProperty), typeof(Color), typeof(NumberButton), Color.White, BindingMode.Default);
+        public static readonly BindableProperty AlternateTextColorProperty = BindableProperty.Create(nameof(AlternateTextColorProperty), typeof(Color), typeof(NumberButton), Color.Black, BindingMode.Default);
+
+
+        public double CornerRadius
+        {
+            get => (double) GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+        public Color AlternateBackgroundColor
+        {
+            get => (Color)GetValue(AlternateBackgroundColorProperty);
+            set => SetValue(AlternateBackgroundColorProperty, value);
+        }
+        public Color TextColor
+        {
+            get => (Color)GetValue(TextColorProperty);
+            set => SetValue(TextColorProperty, value);
+        }
+        public double FontSize
+        {
+            get => (double)GetValue(FontSizeProperty);
+            set => SetValue(FontSizeProperty, value);
+        }
+        public Color AlternateTextColor
+        {
+            get => (Color)GetValue(AlternateTextColorProperty);
+            set => SetValue(AlternateTextColorProperty, value);
+        }
+    }
+}
